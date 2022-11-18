3214bcf7521e2e50f0a5c0755a0d13d79b79486d
アイザック <isaacdarcilla@gmail.com>
2022-11-18 13:32:26 +0800

    pkg: husky

--- app/Services/UsersService.ts
+++ app/Services/UsersService.ts
@@ -4,8 +4,6 @@ export default class UsersService {
 
   /**
    * Get paginated user data
-   * @param param
-   * @returns
    */
   public async all({ page, pagination }): Promise<User[]> {
     return await User.query().paginate(page, pagination);
